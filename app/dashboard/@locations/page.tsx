521f39a4ee77512ab782d5c1751f82a4f204a1d5
LokitoAngel <amgelgabo.d@gmail.com>
2024-10-19 20:47:41 -0600

    Manager implementation

--- app/dashboard/@locations/page.tsx
+++ app/dashboard/@locations/page.tsx
@@ -1,8 +1,8 @@
 import { TOKEN_NAME } from "@/constants";
 import axios from "axios";
 import { cookies } from "next/headers";
-import { Location } from "@/entities";
-import SelectLocation from "./_components/SelectLocation";
+import SelectLocations from "./_components/SelectLocation";
+import LocationCard from "./_components/LocationCard";
 
 const LocationsPage = async ({
   searchParams,
@@ -11,29 +11,31 @@ const LocationsPage = async ({
 }) => {
   const userCookies = cookies();
   const token = userCookies.get(TOKEN_NAME)?.value;
-  let { data } = await axios.get<Location[]>(
-    "http://127.0.0.1:4000/locations",
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }
-  );
+  let { data } = await axios.get("http://127.0.0.1:4000/locations", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
   data = [
     {
       locationId: 0,
       locationName: "Ninguna",
-      locationLatLng: [0.0, 0.0],
+      locationLatLng: [0, 0],
       locationAddress: "No existe",
     },
     ...data,
   ];
-
   return (
     <div className="w-8/12">
       <div className="w-full flex flex-col items-center h-[90vh] bg-red-50">
         <div className="w-1/2 my-10">
-          <SelectLocation locations={data} store={searchParams?.store} />
+          <SelectLocations
+            locations={data}
+            store={searchParams?.store}
+          ></SelectLocations>
+        </div>
+        <div className="w-8/12">
+          <LocationCard store={searchParams.store}></LocationCard>
         </div>
       </div>
     </div>
