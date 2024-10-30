57a91c9c6a3b2fae16bbd0bac5442898e4f1647a
LokitoAngel <amgelgabo.d@gmail.com>
2024-10-29 23:01:25 -0600

    feat: delete manager function

--- actions/managers/delete.ts
+++ actions/managers/delete.ts
@@ -3,13 +3,17 @@
 import { API_URL } from "@/constants";
 import { authHeaders } from "@/helpers/authHeaders";
 import { revalidateTag } from "next/cache";
+import { redirect } from "next/navigation";
 
-export default async function createManager(managerId: string, formData:FormData) {
-    const response = await fetch(`${API_URL}/managers/${managerId}`,{
+export default async function createManager(managerId: string, formData: FormData) {
+    const response = await fetch(`${API_URL}/managers/${managerId}`, {
         method: "DELETE",
-        headers:{
-            ...authHeaders
-        }
-    })
-    if(response.status == 201)revalidateTag("dashboard:managers")
+        headers: {
+            ...authHeaders(),
+        },
+    });
+    if (response.status === 200) {
+        revalidateTag("dashboard:managers");
+        redirect("/dashboard/managers");
+    }
 }
