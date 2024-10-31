f58c30d2da899283b37b8a5df68c56e4e985830d
LokitoAngel <amgelgabo.d@gmail.com>
2024-10-31 00:13:58 -0600

    modal para actulizar manager completado

--- app/dashboard/managers/[id]/_components/FormUpdateManager.tsx
+++ app/dashboard/managers/[id]/_components/FormUpdateManager.tsx
@@ -17,24 +17,39 @@ export default async function FormUpdateManager({
   });
   const stores = await responseStores.json();
   return (
-    <form action={updateManagerwithId} className="bg-orange-400 rounded-md">
+    <form
+      action={updateManagerwithId}
+      className="bg-orange-400 rounded-md flex flex-col flex-grow-0 gap-2"
+    >
       <h1>Actualizar Manager</h1>
       <Input
+        required={true}
+        isRequired
+        label="Nombre completo"
         defaultValue={manager.managerFullName}
         placeholder="Marco Aurelio"
         name="managerFullName"
       />
       <Input
+        required={true}
+        isRequired
+        label="Correo Electronico"
         defaultValue={manager.managerEmail}
         placeholder="manager@ocso.com"
         name="managerEmail"
       />
       <Input
+        required={true}
+        isRequired
+        label="Salario"
         defaultValue={String(manager.managerSalary)}
         placeholder="12000"
         name="managerSalary"
       />
       <Input
+        required={true}
+        isRequired
+        label="Número de teléfono"
         defaultValue={manager.managerPhoneNumber}
         placeholder="4426131618"
         name="managerPhoneNumber"
--- app/dashboard/managers/[id]/_components/SelectStore.tsx
+++ app/dashboard/managers/[id]/_components/SelectStore.tsx
@@ -12,13 +12,15 @@ export default function SelectStore({
 }) {
   const disableStores = stores
     .map((store: Location) => {
-      if (store.manager) {
+      if (store.manager !== undefined && store.locationId !== defaultStore) {
         return String(store.locationId);
       }
     })
     .filter((storeId) => storeId !== undefined);
   return (
     <Select
+      label="Tienda"
+      name="location"
       defaultSelectedKeys={defaultStore ? [defaultStore] : undefined}
       disabledKeys={disableStores}
     >
