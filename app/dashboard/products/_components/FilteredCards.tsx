39b62dc075892ee03b1f131a57bb2512ba77e6bf
LokitoAngel <amgelgabo.d@gmail.com>
2024-11-01 01:29:43 -0600

    feat: Filter Products implementation

---
+++ app/dashboard/products/_components/FilteredCards.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { Product } from "@/entities";
+import Link from "next/link";
+import ProductCard from "./ProductCard";
+import { useEffect, useState } from "react";
+import { Input } from "@nextui-org/react";
+export default function FilteredCards({ products }: { products: Product[] }) {
+  const [filtered, setFiltered] = useState<string>("");
+  const [productsList, setProductsList] = useState<Product[]>(products);
+  useEffect(() => {
+    const filteredProducts = products.filter((product) => {
+      if (product.productName.toLowerCase().includes(filtered.toLowerCase())) {
+        return true;
+      } else return false;
+    });
+    setProductsList(filteredProducts);
+  }, [filtered]);
+  return (
+    <>
+      <div className="max-h-[90vh] min-h-[90vh] overflow-y-auto h-full flex flex-col gap-8 border-r-orange-200 px-10 border-r-2 pt-10">
+        <Input
+          onChange={(e) => {
+            setFiltered(e.target.value);
+          }}
+          label="Nombre del producto"
+        />
+        {productsList.map((product) => {
+          return (
+            <Link
+              key={product.productId}
+              href={{ pathname: `/dashboard/products/${product.productId}` }}
+            >
+              <ProductCard product={product} />
+            </Link>
+          );
+        })}
+      </div>
+    </>
+  );
+}
--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,4 +1,21 @@
-const ProductsPage = () => {
-  return null;
+import { API_URL } from "@/constants";
+import { Product } from "@/entities";
+import { authHeaders } from "@/helpers/authHeaders";
+import FilteredCards from "./_components/FilteredCards";
+const ProductsPage = async () => {
+  const response = await fetch(`${API_URL}/products`, {
+    method: "GET",
+    headers: { ...authHeaders() },
+    next: { tags: ["dashboard:products"] },
+  });
+  const products: Product[] = await response.json();
+  return (
+    <div className="h-[90vh] w-full">
+      <div className="w-4/12">
+        <FilteredCards products={products} />
+      </div>
+    </div>
+  );
 };
+
 export default ProductsPage;
